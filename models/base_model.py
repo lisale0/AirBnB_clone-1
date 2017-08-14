3ff74950e295af48c3be62de0214300b03588352
root <110@holbertonschool.com>
2017-08-14 21:26:11 +0000

    updated basemodel to handle for sqlalchemy

--- models/base_model.py
+++ models/base_model.py
@@ -2,27 +2,36 @@
 """
 BaseModel Class of Models Module
 """
-
+import os
 import json
 import models
 from uuid import uuid4, UUID
 from datetime import datetime
 
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+
 now = datetime.now
 strptime = datetime.strptime
 
+if (os.getenv('HBNB_TYPE_STORAGE') == 'db'):
+    Base = declarative_base()
 
 class BaseModel:
-    """attributes and functions for BaseModel class"""
+    """attributes and functions for BaseModel class""" 
+    if (os.getenv('HBNB_TYPE_STORAGE') == 'db'):
+        id = Column(Integer, nullable=False, primary_key=True)
+        created_at = Column(DateTime, default=datetime.utcnow, nullable=False)
+        updated_at = Column(DateTime, default=datetime.utcnow, nullable=False)
 
     def __init__(self, *args, **kwargs):
         """instantiation of new BaseModel Class"""
+        
         if kwargs:
             self.__set_attributes(kwargs)
         else:
             self.id = str(uuid4())
             self.created_at = now()
-            models.storage.new(self)
 
     def __set_attributes(self, d):
         """converts kwargs values to python class attributes"""
@@ -57,8 +66,8 @@ class BaseModel:
     def save(self):
         """updates attribute updated_at to current time"""
         self.updated_at = now()
+        models.storage.new(self)
         models.storage.save()
-
     def to_json(self):
         """returns json representation of self"""
         bm_dict = {}
@@ -74,3 +83,7 @@ class BaseModel:
         """returns string type representation of object instance"""
         cname = type(self).__name__
         return "[{}] ({}) {}".format(cname, self.id, self.__dict__)
+
+    def delete(self):
+        models.storage.delete(self)
+
