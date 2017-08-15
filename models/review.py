14630f894036a39c6b175b598ca361bcd3f854f7
root <110@holbertonschool.com>
2017-08-15 04:42:03 +0000

    adding review for ORM

--- models/review.py
+++ models/review.py
@@ -2,16 +2,22 @@
 """
 Review Class from Models Module
 """
+import os
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey
 
-from models.base_model import BaseModel
-
-
-class Review(BaseModel):
+class Review(BaseModel, Base):
     """Review class handles all application reviews"""
+    __tablename__ = 'reviews'
+    if (os.getenv('HBNB_TYPE_STORAGE') == 'db'):
+        text = Column(String(1024), nullable=False)
+        place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
+        user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
 
-    place_id = ''
-    user_id = ''
-    text = ''
+    else:
+        place_id = ''
+        user_id = ''
+        text = ''
 
     def __init__(self, *args, **kwargs):
         """instantiates a new review"""
