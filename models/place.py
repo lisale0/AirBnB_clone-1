56bf5d1cd8c2678c7e4528d8e826614a5c38f534
root <110@holbertonschool.com>
2017-08-15 04:19:47 +0000

    makes both place_id and amenity_id primary keys

--- models/place.py
+++ models/place.py
@@ -9,14 +9,16 @@ from sqlalchemy import Column, String, Integer, ForeignKey, Float
 from sqlalchemy.orm import relationship
 
 class PlaceAmenty(Base):
-    __table__ = 'place_amenity'
-    place_id = Column(String(60), ForeignKey("places.id"),  nullable=False)
-    amenity_id = Column(String(60), ForeignKey("amenities.id"), nullable=False)
+    __tablename__ = 'place_amenity'
+    place_id = Column(String(60), ForeignKey('places.id'), primary_key=True, nullable=False)
+    amenity_id = Column(String(60), ForeignKey("amenities.id"), primary_key=True, nullable=False)
 
 class Place(BaseModel, Base):
     """Place class handles all application places"""
+
     if (os.getenv('HBNB_TYPE_STORAGE') == 'db'):
         __tablename__ = "places"
+
         city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
         user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
         name = Column(String(128), nullable=False)
@@ -28,7 +30,9 @@ class Place(BaseModel, Base):
         latitude = Column(Float, nullable=True)
         longitude = Column(Float, nullable=True)
         amenities = relationship("Amenity", secondary="place_amenity", viewonly=False)
+        """
         reviews = relationship("Review", backref="place")
+        """
     else:
         city_id = ''
         user_id = ''
