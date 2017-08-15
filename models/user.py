77e15797aa8c7bce9ab9d4342cf535ced72694b3
root <110@holbertonschool.com>
2017-08-15 05:56:14 +0000

    ORM for user

--- models/user.py
+++ models/user.py
@@ -8,11 +8,21 @@ from models.base_model import BaseModel
 
 class User(BaseModel):
     """User class handles all application users"""
+    if (os.getenv("HBNB_TYPE_STORAGE") == "db"):
+        __tablename__ = "users"
 
-    email = ''
-    password = ''
-    first_name = ''
-    last_name = ''
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=False)
+        last_name = Column(String(128), nullable=False)
+        places = relationship('Place', backref="user")
+        reviews = relationship('Review', backref="user")
+
+    else:
+        email = ''
+        password = ''
+        first_name = ''
+        last_name = ''
 
     def __init__(self, *args, **kwargs):
         """instantiates a new user"""
