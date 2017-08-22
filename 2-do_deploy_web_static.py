812370c6d618d7f2a3f3a9f11e58c645f0eb3cb6
root <110@holbertonschool.com>
2017-08-22 04:56:18 +0000

    2 initial add

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -33,7 +33,6 @@ def do_deploy(archive_path):
 
     """remove"""
     run("sudo rm -rf /data/web_static/releases/{}/web_static".format(filename))
-
     
     run("sudo rm -rf /data/web_static/current")
     run("sudo ln -s /data/web_static/releases/{}/ /data/web_static/current".format(filename))
