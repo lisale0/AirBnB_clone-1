8c78d8061b4f1a9ef2b1b94de925f4af6bd7f989
root <110@holbertonschool.com>
2017-08-22 05:22:53 +0000

    pep8 fix

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" 
+"""
 deploy
 """
 import time
@@ -15,6 +15,9 @@ def do_deploy(archive_path):
     if not archive_path:
         return False
 
+    if not os.path.exists(archive_path):
+        return False
+
     filename = archive_path.split("/")[-1]
     """upload the archive to the /tmp/ directory of the web server"""
     put(archive_path, "/tmp/{}".format(filename))
@@ -23,17 +26,22 @@ def do_deploy(archive_path):
     run("sudo mkdir -p /data/web_static/releases/{}".format(filename))
 
     """uncompress the archive to the folder"""
-    run("sudo tar -xzf /tmp/{} -C /data/web_static/releases/{}".format(filename, filename))
+    run("sudo tar -xzf /tmp/{} -C /data/web_static/releases/{}"
+        .format(filename, filename))
 
     """ remove tgz file"""
     run("sudo rm /tmp/{}".format(filename))
 
     """ move all the files"""
-    run("sudo mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}".format(filename, filename))
+    run("sudo mv /data/web_static/releases/{}/web_static/*"
+        " /data/web_static/releases/{}"
+        .format(filename, filename))
 
     """remove"""
-    run("sudo rm -rf /data/web_static/releases/{}/web_static".format(filename))
-    
+    run("sudo rm -rf /data/web_static/releases/{}/web_static"
+        .format(filename))
+
     run("sudo rm -rf /data/web_static/current")
-    run("sudo ln -s /data/web_static/releases/{}/ /data/web_static/current".format(filename))
+    run("sudo ln -s /data/web_static/releases/{}/ /data/web_static/current"
+        .format(filename))
     print("New version deployed!")
