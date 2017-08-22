a9015eac35865066578fc6505cbe0454be3ff483
root <110@holbertonschool.com>
2017-08-22 05:07:21 +0000

    pep8 fix

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -8,6 +8,7 @@ from fabric.api import local, run, hosts, env, put
 
 env.hosts = ['144.217.245.116', '54.91.123.178']
 
+
 def do_pack():
     """
     do_pack - method that compresses the files
@@ -37,20 +38,26 @@ def do_deploy(archive_path):
     run("sudo mkdir -p /data/web_static/releases/{}".format(filename))
 
     """uncompress the archive to the folder"""
-    run("sudo tar -xzf /tmp/{} -C /data/web_static/releases/{}".format(filename, filename))
+    run("sudo tar -xzf /tmp/{} -C /data/web_static/releases/{}"
+        .format(filename, filename))
 
     """ remove tgz file"""
     run("sudo rm /tmp/{}".format(filename))
 
     """ move all the files"""
-    run("sudo mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}".format(filename, filename))
+    run("sudo mv /data/web_static/releases/{}/web_static/*"
+        " /data/web_static/releases/{}"
+        .format(filename, filename))
 
     """remove"""
-    run("sudo rm -rf /data/web_static/releases/{}/web_static".format(filename))
+    run("sudo rm -rf /data/web_static/releases/{}/web_static"
+        .format(filename))
 
     run("sudo rm -rf /data/web_static/current")
-    run("sudo ln -s /data/web_static/releases/{}/ /data/web_static/current".format(filename))
+    run("sudo ln -s /data/web_static/releases/{}/ /data/web_static/current"
+        .format(filename))
     print("New version deployed!")
+
 
 def deploy():
     """
