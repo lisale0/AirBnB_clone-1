fdde4d56ae7a3b9187260466c7f70dc3e9cab6e7
root <110@holbertonschool.com>
2017-08-22 04:27:33 +0000

    pep8 fix

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
-""" 
+"""
 Compress file
 """
 import time
 from fabric.api import local, run, hosts, env
+
 
 def do_pack():
     """
     do_pack - method that compresses the files
     """
     deploy_time = time.strftime("%Y%m%d%H%M%S")
-    new_tar_file = "web_static_" + deploy_time + ".tgz" 
+    new_tar_file = "web_static_" + deploy_time + ".tgz"
     try:
         local("mkdir -p versions")
         local("tar -cvzf versions/{} web_static".format(new_tar_file))
