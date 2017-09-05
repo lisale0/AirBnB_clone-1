637031fb49bd48042dd3c112533927d341e25276
Lisa Leung <110@holbertonschool.com>
2017-09-05 23:18:20 +0000

    initial

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""a script that starts a Flask web application """
+from flask import Flask
+
+app = Flask(__name__)
+
+""" Route: / """
+@app.route('/', strict_slashes=False)
+def display_hello_hbnb():
+    return 'Hello HBNB!'
+
+""" Route: /hbnb """
+@app.route('/hbnb', strict_slashes=False)
+def display_hbnb():
+    return 'HBNB'
+
+""" Route: /c/<string:text> """
+@app.route('/c/<string:text>', strict_slashes=False)
+def display_c(text):
+    text = text.replace("_"," ")
+    return 'C {}'.format(text)
+
+""" Route: /python/* """
+@app.route('/python', strict_slashes=False)
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<string:text>', strict_slashes=False)
+def display_python(text="is cool"):
+    text = text.replace("_"," ")
+    return 'Python {}'.format(text)
+
+""" Route: /number/<int:num> """
+@app.route('/number/<int:num>', strict_slashes=False)
+def display_number(num):
+    return '{} is a number'.format(num)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
