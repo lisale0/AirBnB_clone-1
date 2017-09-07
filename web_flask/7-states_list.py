994849faede401a69f7f58328341f7e22487485e
Lisa Leung <110@holbertonschool.com>
2017-09-07 19:40:49 +0000

    testing stuart's files

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,30 +1,23 @@
 #!/usr/bin/python3
-"""a script that starts a Flask web application """
-from flask import Flask
-from flask import render_template
+"""
+7-states_list - starts a flask application
+"""
+from flask import Flask, render_template
 from models import storage
-
 app = Flask(__name__)
-
-""" after each request is done, close storage session"""
 
 
 @app.teardown_appcontext
-def close_session(exception):
-    """close storage"""
+def teardown(self):
+    """  close storage session """
     storage.close()
-
-""" Route: /states_list """
 
 
 @app.route('/states_list', strict_slashes=False)
-def state_list():
-    state_list = storage.all("State")
-    state_arr = state_list.values()
-    sorted_arr = []
-    for state in sorted(state_arr, key=lambda k: k.name):
-        sorted_arr.append(state)
-    return render_template('7-states_list.html', state_list=sorted_arr)
+def states_list():
+    """ injects states list into html  """
+    states = storage.all('State').values()
+    return (render_template('7-states_list.html', states=states))
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
